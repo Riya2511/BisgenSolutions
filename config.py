be7bd4fe72e226b09eb446eaf53116fc349dc1d8
Jay Thorat <jaythorat.dev@gmail.com>
2024-11-17 19:19:12 +0530

    added few comments

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ SQL_CONFIG = {
     "port": 3306,
     "user": "root",
     "password": "Qwerty@123",
-    "database": "test"
+    "database": "test",
 }
 
 BATCH_SIZE = 50  # Number of emails to fetch in each batch
-CRON_INTERVAL = 5
+CRON_INTERVAL = 5  # Interval in seconds
 EMAIL_PROCESS_BATCH_SIZE = 1
+
